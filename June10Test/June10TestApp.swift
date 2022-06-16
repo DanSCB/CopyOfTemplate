a49d986b709acc218b314be2b3518d7641a6ae4f
DanSCB <dans@clearbridgemobile.com>
2022-06-16 11:10:56 -0400

    Initial commit

---
+++ June10Test/June10TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  June10TestApp.swift
+//  June10Test
+//
+//  Created by Danny Schnabel on 2022-06-10.
+//
+
+import SwiftUI
+
+@main
+struct June10TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# June10Test
+
+This is comparable to a template project with the SDK brought in as an unalterable dependency.
